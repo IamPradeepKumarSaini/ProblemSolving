e100b986816673b01873fe0f805345e0dac22891
KuPradeep <swer.pradeep@yahoo.co.in>
2019-11-16 12:44:29 +0530

    UP

--- src/main/java/ArrayListTest.java
+++ src/main/java/ArrayListTest.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class ArrayListTest {
+    public static void main(String args[]){
+        ArrayList<String> list=new ArrayList<String>();//Creating arraylist
+        list.add("Ravi");//Adding object in arraylist
+        list.add("Vijay");
+        list.add("hell");
+        list.add("Ajay");
+        list.add("Bill");
+        list.add("Geo");
+
+
+        System.out.println("Traversing list through Iterator:");
+        //Traversing list through Iterator
+        Iterator itr=list.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
 }
